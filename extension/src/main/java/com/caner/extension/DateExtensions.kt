ae9c5d51247db8c7cf26d4a88a96bc46fb541860
ccanergulgec <caner@armut.com>
2020-04-21 14:55:04 +0300

    Add relativeTimeSpanString method

--- extension/src/main/java/com/caner/extension/DateExtensions.kt
+++ extension/src/main/java/com/caner/extension/DateExtensions.kt
@@ -7,3 +7,15 @@ import android.widget.Toast
 fun toast(c: Context?, message: String?) {
     Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
 }
+
+/**
+ * Returns a string describing the elapsed time since startTime.
+ * @param startTime some time in the past.
+ * @return a String object containing the elapsed time.
+ * @see .getRelativeTimeSpanString
+ */
+fun getRelativeTimeSpanString(startTime: Long): CharSequence? {
+    return getRelativeTimeSpanString(
+        startTime
+    )
+}
