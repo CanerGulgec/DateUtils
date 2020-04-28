cc935e537e2aa0aea986085b3a7f4154a5fc22c5
ccanergulgec <caner@armut.com>
2020-04-28 14:18:00 +0300

    Add fromMillisToTimeString date method

--- extension/src/main/java/com/caner/extension/DateExtensions.kt
+++ extension/src/main/java/com/caner/extension/DateExtensions.kt
@@ -1,27 +1,51 @@
 package com.caner.extension
 
-import android.text.format.DateUtils.HOUR_IN_MILLIS
-import android.text.format.DateUtils.MINUTE_IN_MILLIS
+import android.text.format.DateUtils
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-const val SECOND_IN_MILLIS: Long = 1000
-const val MINUTE_IN_MILLIS = SECOND_IN_MILLIS * 60
-const val HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60
-const val DAY_IN_MILLIS = HOUR_IN_MILLIS * 24
-
-val YMDHMS = "yyyy-MM-dd HH:mm:ss"
-val YMDHM = "yyyy-MM-dd HH:mm"
-val YMD = "yyyy-MM-dd"
-
-
-private lateinit var DATE_FORMAT: SimpleDateFormat
+//TODO RETURN WITH UTC
 
 /**
- * This method returns formatted time to display in selected format
+ * This method returns current date in specified pattern
  */
-fun fromMillisToTimeString(millis: Long, pattern: String = YMDHM): String {
-    DATE_FORMAT = SimpleDateFormat(pattern, Locale.getDefault())
-    return DATE_FORMAT.format(millis)
+fun getCurrentDate(pattern: String = DATE_FORMAT_3): String =
+    SimpleDateFormat(pattern, Locale.getDefault()).format(Date())
+
+/**
+ * This method returns formatted time to display in specified pattern
+ */
+fun getDateString(timeInMillis: Long, pattern: String = DATE_FORMAT_3): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale.getDefault())
+    return dateFormat.format(timeInMillis)
+}
+
+fun getDateStringWithSpecificMessage(timeInMillis: Long, pattern: String = DATE_FORMAT_3): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale.getDefault())
+
+    return when {
+        isToday(timeInMillis) -> "Today"
+        isTomorrow(timeInMillis) -> "Tomorrow"
+        isYesterday(timeInMillis) -> "Yesterday"
+        else -> dateFormat.format(timeInMillis)
+    }
+}
+
+fun getMilliSecondsFromDateString(dateString: String, pattern: String): Long? {
+    val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+    val mDate = sdf.parse(dateString)
+    return mDate?.time
+}
+
+private fun isYesterday(whenInMillis: Long): Boolean {
+    return DateUtils.isToday(whenInMillis + DateUtils.DAY_IN_MILLIS)
+}
+
+fun isTomorrow(whenInMillis: Long): Boolean {
+    return DateUtils.isToday(whenInMillis - DateUtils.DAY_IN_MILLIS)
+}
+
+private fun isToday(whenInMillis: Long): Boolean {
+    return DateUtils.isToday(whenInMillis)
 }
